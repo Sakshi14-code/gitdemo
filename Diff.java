7e81645925edcb85a43053090893d81629400a3d
Sakshi Sinha <sakshiryan3@gmail.com>
2022-06-04 21:42:50 +0530

    Diff.java

--- Diff.java
+++ Diff.java
@@ -1,6 +1,7 @@
 public class Diff {
-public static void main(String[] args){
-    
-}    
-    
-}
+    public static void main(String[] args){
+    int a=10;
+    int b=20;
+    System.out.println("The difference is"+(a-b));    
+    }    
+    }    
